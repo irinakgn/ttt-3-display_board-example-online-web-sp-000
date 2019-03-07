f9b393d853a00367b03cb4a26894e0b70b22fe9e
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-07 01:13:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,3 @@
 def display_board# Define a method display_board that prints a 3x3 Tic Tac Toe Board
-put display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+
 end
